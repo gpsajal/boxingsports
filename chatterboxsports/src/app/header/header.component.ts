d32bb9471e69a9468fd76f1e1be41b787b65ad8b
gpsajal <sajaltesting@gmail.com>
2021-01-08 23:28:25 +0530

    Worked on add angular material and create singin and signup dialog

--- chatterboxsports/src/app/header/header.component.ts
+++ chatterboxsports/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SigninComponent } from '../signin/signin.component';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openSigninDialog()
+  {
+    const dialogRef = this.dialog.open(SigninComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
