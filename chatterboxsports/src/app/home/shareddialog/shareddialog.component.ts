2e7fa473f0d7ab9abbdf7b46637c6604de48af8a
gpsajal <sajaltesting@gmail.com>
2021-01-24 12:51:51 +0530

    worked on video dialog

--- chatterboxsports/src/app/home/shareddialog/shareddialog.component.ts
+++ chatterboxsports/src/app/home/shareddialog/shareddialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 @Component({
   selector: 'app-shareddialog',
@@ -8,14 +8,19 @@ import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 })
 export class ShareddialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public shareddata: any,private _sanitizer: DomSanitizer) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public shareddata: any,private _sanitizer: DomSanitizer,public dialog: MatDialog) { }
   videoUrl:any;
   ngOnInit(): void {
     if(this.shareddata.videoUrl != undefined && this.shareddata.videoUrl != '')
     {
       this.videoUrl = this._sanitizer.bypassSecurityTrustResourceUrl(this.shareddata.videoUrl);
-      console.log(this.videoUrl);
+      //console.log(this.videoUrl);
     }
   }
 
+
+  closeDialog()
+  {
+    this.dialog.closeAll();
+  }
 }
