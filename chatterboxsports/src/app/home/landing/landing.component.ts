2962a50393860f6242bd7072b0ae31706b193ac4
gpsajal <sajaltesting@gmail.com>
2021-01-26 01:33:04 +0530

    worked on issues

--- chatterboxsports/src/app/home/landing/landing.component.ts
+++ chatterboxsports/src/app/home/landing/landing.component.ts
@@ -14,8 +14,10 @@ export class LandingComponent implements OnInit {
 
   constructor(public dialog: MatDialog,private alertService:AlertService,private homeService:HomeService) { }
   liveChannelVideos:any = [];
+  liveChannelVideosData:any = [];
   instantClassicChannelVideos:any = [];
   recentVideos:any = [];
+  recentVideosData:any = [];
   loader:boolean =false;
   totalLiveVideos:any;
   totalInstantVideos:any;
@@ -24,7 +26,7 @@ export class LandingComponent implements OnInit {
   isSeeAllInstant:boolean = false;
   isSeeAllRecent:boolean = false;
   ngOnInit(): void {
-    this.getLiveChannelData(0,6);
+    this.getLiveChannelData(0,1000);
     this.getInstantClassicChannelData(0,6);
   }
 
@@ -47,26 +49,21 @@ export class LandingComponent implements OnInit {
   /* End-  function for open video dialog*/
 
   /* Start- function for get live channel videos*/
-  getLiveChannelData(page,limit,isSeeAll=''){
+  getLiveChannelData(page,limit){
     this.loader = true;
-    if(isSeeAll != '')
-    {
-      this.isSeeAll = true;
-    }
-    else
-    {
-      this.isSeeAll = false;
-    }
     this.homeService.getLiveChannel('1',page,limit).subscribe(
       response => {
         this.loader = false;
         if (response != undefined) {
-          //console.log(response);
-           this.liveChannelVideos = response.data;
-           this.totalLiveVideos = response.total_records
+          console.log(response);
+          this.liveChannelVideosData = response.data.live;
+           this.liveChannelVideos = this.liveChannelVideosData.slice(0,6);
+           this.totalLiveVideos = this.liveChannelVideosData.length;
            for(var i = 0; i<this.liveChannelVideos.length; i++)
            {
             this.liveChannelVideos[i].viewer_url = environment.BOXCAST_VIEWER_URL+this.liveChannelVideos[i].channel_id;
+            this.liveChannelVideos[i].starts_at = moment.utc(this.liveChannelVideos[i].starts_at).local().format(environment.DATE_TIME_FORMAT);
+            this.liveChannelVideos[i].stops_at = moment.utc(this.liveChannelVideos[i].stops_at).local().format(environment.DATE_TIME_FORMAT);
            }
            //console.log(this.liveChannelVideos);
            this.getRecentGamesData(0,1000);
@@ -96,14 +93,14 @@ export class LandingComponent implements OnInit {
         if (response != undefined) {
           //console.log(response);
            this.instantClassicChannelVideos = response.data;
-           this.totalInstantVideos = response.total_records
+           this.totalInstantVideos = this.instantClassicChannelVideos.length;
            //console.log(this.instantClassicChannelVideos);
            for(var i = 0; i<this.instantClassicChannelVideos.length; i++)
            {
             this.instantClassicChannelVideos[i].durationTime = this.convertSecondsToHms(this.instantClassicChannelVideos[i].recording_duration_seconds);
             this.instantClassicChannelVideos[i].viewer_url = environment.BOXCAST_VIEWER_URL+this.instantClassicChannelVideos[i].channel_id;
-            this.instantClassicChannelVideos[i].starts_at = moment(this.instantClassicChannelVideos[i].starts_at).format(environment.DATE_TIME_FORMAT);
-            this.instantClassicChannelVideos[i].stops_at = moment(this.instantClassicChannelVideos[i].stops_at).format(environment.DATE_TIME_FORMAT);
+            this.instantClassicChannelVideos[i].starts_at = moment.utc(this.instantClassicChannelVideos[i].starts_at).local().format(environment.DATE_TIME_FORMAT);
+            this.instantClassicChannelVideos[i].stops_at = moment.utc(this.instantClassicChannelVideos[i].stops_at).local().format(environment.DATE_TIME_FORMAT);
            }
            //console.log(this.instantClassicChannelVideos);
         }
@@ -131,37 +128,63 @@ export class LandingComponent implements OnInit {
   }
 
   /* Start- function for get recent games videos*/
-  getRecentGamesData(page,limit,isSeeAllRecent=''){
+  getRecentGamesData(page,limit){
     this.loader = true;
-    if(isSeeAllRecent != '')
-    {
-      this.isSeeAllRecent = true;
-    }
-    else
-    {
-      this.isSeeAllRecent = false;
-    }
-    this.homeService.getLiveChannel('4',page,limit).subscribe(
+    this.homeService.getLiveChannel('1',page,limit).subscribe(
       response => {
         this.loader = false;
         if (response != undefined) {
           //console.log(response);
-           this.recentVideos = response.data;
-           this.totalRecentVideos = response.total_records
+          this.recentVideosData = response.data.recent;
+           this.recentVideos = this.recentVideosData.slice(0,6);
+           this.totalRecentVideos = this.recentVideosData.length;
            for(var i = 0; i<this.recentVideos.length; i++)
            {
             this.recentVideos[i].viewer_url = environment.BOXCAST_VIEWER_URL+this.recentVideos[i].channel_id;
-            this.recentVideos[i].starts_at = moment(this.recentVideos[i].starts_at).format(environment.DATE_TIME_FORMAT);
-            this.recentVideos[i].stops_at = moment(this.recentVideos[i].stops_at).format(environment.DATE_TIME_FORMAT);
+            this.recentVideos[i].starts_at = moment.utc(this.recentVideos[i].starts_at).local().format(environment.DATE_TIME_FORMAT);
+            this.recentVideos[i].stops_at = moment.utc(this.recentVideos[i].stops_at).local().format(environment.DATE_TIME_FORMAT);
            }
            //console.log(this.recentVideos);
         }
       },
     error => {
       this.loader = false;
-      this.isSeeAllRecent = false;
+      //this.isSeeAllRecent = false;
     });
   }
   /* End- function for get recent games videos*/
+
+  seeAllRecent(isSeeAllRecent)
+  {
+    this.loader = true;
+    if(isSeeAllRecent == 'true')
+    {
+      this.isSeeAllRecent = true;
+      this.recentVideos = this.recentVideosData;
+    }
+    else
+    {
+      this.isSeeAllRecent = false;
+      this.recentVideos = this.recentVideosData.slice(0,6);
+    }
+    setTimeout(()=>{this.loader = false; },2000);
+  }
+
+  seeAllLive(isSeeAll)
+  {
+    this.loader = true;
+    if(isSeeAll == 'true')
+    {
+      this.isSeeAll = true;
+      this.liveChannelVideos = this.liveChannelVideosData;
+    }
+    else
+    {
+      this.isSeeAll = false;
+      this.liveChannelVideos = this.liveChannelVideosData.slice(0,6);
+    }
+    setTimeout(()=>{this.loader = false; },2000);
+  }
+
 
 }
--- chatterboxsports/src/app/home/liveplus/liveplus.component.ts
+++ chatterboxsports/src/app/home/liveplus/liveplus.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ShareddialogComponent } from '../shareddialog/shareddialog.component';
-import {MatDialog} from '@angular/material/dialog';
 import { AlertService, AuthenticationService }  from '../../common/index';
 import { HomeService } from '../home.service';
 import { environment } from 'src/environments/environment';
+import { SignupComponent } from '../../user/signup/signup.component';
+import {MatDialog , MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import * as moment from 'moment'
 @Component({
   selector: 'app-liveplus',
@@ -12,7 +13,9 @@ import * as moment from 'moment'
 })
 export class LiveplusComponent implements OnInit {
   livePlusChannelVideos:any = [];
+  livePlusChannelVideosData:any = [];
   recentVideos:any = [];
+  recentVideosData:any = [];
   loader:boolean =false;
   totalLivePlusVideos:any;
   totalRecentVideos:any;
@@ -60,12 +63,15 @@ export class LiveplusComponent implements OnInit {
         this.loader = false;
         if (response != undefined) {
           //console.log(response);
-           this.livePlusChannelVideos = response.data;
-           this.totalLivePlusVideos = response.total_records;
+          this.livePlusChannelVideosData = response.data.live;
+           this.livePlusChannelVideos = this.livePlusChannelVideosData.slice(0,6);
+           this.totalLivePlusVideos = this.livePlusChannelVideosData.length;
            for(var i = 0; i<this.livePlusChannelVideos.length; i++)
            {
             //this.livePlusChannelVideos[i].durationTime = this.convertSecondsToHms(this.livePlusChannelVideos[i].recording_duration_seconds);
             this.livePlusChannelVideos[i].viewer_url = environment.BOXCAST_VIEWER_URL+this.livePlusChannelVideos[i].channel_id;
+            this.livePlusChannelVideos[i].starts_at = moment.utc(this.livePlusChannelVideos[i].starts_at).local().format(environment.DATE_TIME_FORMAT);
+            this.livePlusChannelVideos[i].stops_at = moment.utc(this.livePlusChannelVideos[i].stops_at).local().format(environment.DATE_TIME_FORMAT);
            }
            //console.log(this.livePlusChannelVideos);
            this.getRecentGamesData(0,1000);
@@ -97,45 +103,88 @@ export class LiveplusComponent implements OnInit {
     }
     else
     {
-       this.alertService.info('Please login to watch live+ videos.');
+       //this.alertService.info('Please login to watch live+ videos.');
+       this.openSignupDialog();
        return false;
     }
   }
   /* End-  function for open video dialog*/
 
   /* Start- function for get recent games videos*/
-  getRecentGamesData(page,limit,isSeeAllRecent=''){
+  getRecentGamesData(page,limit){
     this.loader = true;
-    if(isSeeAllRecent != '')
-    {
-      this.isSeeAllRecent = true;
-    }
-    else
-    {
-      this.isSeeAllRecent = false;
-    }
-    this.homeService.getLiveChannel('5',page,limit).subscribe(
+    this.homeService.getLiveChannel('3',page,limit).subscribe(
       response => {
         this.loader = false;
         if (response != undefined) {
           //console.log(response);
-           this.recentVideos = response.data;
-           this.totalRecentVideos = response.total_records
+          this.recentVideosData = response.data.recent;
+           this.recentVideos = this.recentVideosData.slice(0,6);
+           this.totalRecentVideos = this.recentVideosData.length;
            for(var i = 0; i<this.recentVideos.length; i++)
            {
             this.recentVideos[i].viewer_url = environment.BOXCAST_VIEWER_URL+this.recentVideos[i].channel_id;
-            this.recentVideos[i].starts_at = moment(this.recentVideos[i].starts_at).format(environment.DATE_TIME_FORMAT);
-            this.recentVideos[i].stops_at = moment(this.recentVideos[i].stops_at).format(environment.DATE_TIME_FORMAT);
+            this.recentVideos[i].starts_at = moment.utc(this.recentVideos[i].starts_at).local().format(environment.DATE_TIME_FORMAT);
+            this.recentVideos[i].stops_at = moment.utc(this.recentVideos[i].stops_at).local().format(environment.DATE_TIME_FORMAT);
            }
            //console.log(this.recentVideos);
         }
       },
     error => {
       this.loader = false;
-      this.isSeeAllRecent = false;
+      //this.isSeeAllRecent = false;
     });
   }
   /* End- function for get recent games videos*/
+
+  seeAllRecent(isSeeAllRecent)
+  {
+    this.loader = true;
+    if(isSeeAllRecent == 'true')
+    {
+      this.isSeeAllRecent = true;
+      this.recentVideos = this.recentVideosData;
+    }
+    else
+    {
+      this.isSeeAllRecent = false;
+      this.recentVideos = this.recentVideosData.slice(0,6);
+    }
+    setTimeout(()=>{this.loader = false; },2000);
+  }
+
+  seeAllLive(isSeeAll)
+  {
+    this.loader = true;
+    if(isSeeAll == 'true')
+    {
+      this.isSeeAll = true;
+      this.livePlusChannelVideos = this.livePlusChannelVideosData;
+    }
+    else
+    {
+      this.isSeeAll = false;
+      this.livePlusChannelVideos = this.livePlusChannelVideosData.slice(0,6);
+    }
+    setTimeout(()=>{this.loader = false; },2000);
+  }
+
+
+  openSignupDialog()
+  {
+    this.dialog.closeAll();
+    const dialogRef = this.dialog.open(SignupComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      //console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  closeDialog()
+  {
+    this.dialog.closeAll();
+  }
+
 
 
 }
