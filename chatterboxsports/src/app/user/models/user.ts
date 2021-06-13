4f170a91cddfe2633387c7cbf4b74e7745aae9ad
gpsajal <sajaltesting@gmail.com>
2021-06-13 23:53:44 +0530

    change user model

--- chatterboxsports/src/app/user/models/user.ts
+++ chatterboxsports/src/app/user/models/user.ts
@@ -1,29 +1,5 @@
 ﻿export class User {
-    id: number;
-    userId: any;
-    fullname: string;
+    first_name: string;
+    last_name: string;
     email: string;
-    mobile: number;
-    phonecountrycode:number;
-    currentpassword: string;
-    newpassword: string;
-    confirmpassword: string;
-    terms:boolean;
-    newsletter:boolean;
-    gender: string;
-    birthdate: Date;
-    timezoneid: number;
-    timezonename: string;
-    businessname: string;
-    taxnumber: string;
-    domain: string;
-    license_number:string;
-    registration_exp_date:string;
-    manageable_properties:any;
-    appointment_docs:any;
-    company_registration_docs:any;
-    inputprofileimage: string;
-    tellus:string;
-    details:string;
-
 }
